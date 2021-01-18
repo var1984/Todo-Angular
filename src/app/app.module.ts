5171736c68759d35c137d5011fc3acc506e9b5b8
Bogdan Oshovski <bodyaua1984@gmail.com>
2021-01-18 13:01:08 +0200

    Добавлены компонент и сервис categories(пустые)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CategoriesComponent } from './views/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
